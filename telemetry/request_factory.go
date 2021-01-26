00a66f50560ce3200b01162136e90e2d7facca1d
Allan Feldman <afeldman@newrelic.com>
2021-01-26 15:06:29 -0500

    Update request factory.

--- telemetry/request_factory.go
+++ telemetry/request_factory.go
@@ -1,14 +1,24 @@
 package telemetry
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
-type DataPoint struct {}
+type DataPoint interface {}
+
+type Batch interface {
+	Type() string
+	Bytes() *bytes.Buffer
+	DataPoints() []DataPoint
+}
 
 type RequestFactory interface {
-	BuildRequest([]DataPoint, ...ClientOption) http.Request
+	BuildRequest(Batch, ...ClientOption) http.Request
 }
 
 type requestFactory struct {
@@ -18,8 +28,58 @@ type requestFactory struct {
 	port uint
 }
 
-func (f requestFactory) BuildRequest(points []DataPoint, option ...ClientOption) http.Request {
-	panic("implement me")
+func (f *requestFactory) BuildRequest(batch Batch, options ...ClientOption) http.Request {
+	configuredFactory := requestFactory{
+		insertKey:    f.insertKey,
+		noDefaultKey: f.noDefaultKey,
+		host:         f.host,
+		port:         f.port,
+	}
+
+	for _, option := range options {
+		option(&configuredFactory)
+	}
+
+	bytes := batch.Bytes()
+	body := ioutil.NopCloser(bytes)
+	host := configuredFactory.getHost()
+	headers := configuredFactory.getHeaders()
+	path := configuredFactory.getPath(batch.Type())
+
+	return http.Request{
+		Method:           "POST",
+		URL:              &url.URL{
+			Scheme:      "https",
+			Host:        configuredFactory.host,
+			Path:        path,
+		},
+		Header:           headers,
+		Body:             body,
+		ContentLength:    int64(bytes.Len()),
+		Close:            false,
+		Host:             host,
+	}
+}
+
+func (f *requestFactory) getPath(t string) string {
+	switch t {
+	case "spans": return "/trace/v1"
+	case "metrics": return "/metric/v1"
+	case "logs": return "/log/v1"
+	default: return ""
+	}
+}
+
+func (f *requestFactory) getHost() string {
+	s := f.host
+	if f.port > 0 {
+		s = s + fmt.Sprintf(":%d", f.port)
+	}
+	return s
+}
+
+func (f *requestFactory) getHeaders() http.Header {
+	return http.Header{}
 }
 
 type ClientOption func(o *requestFactory)
