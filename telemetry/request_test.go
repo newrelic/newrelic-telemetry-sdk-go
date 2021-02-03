5e630ec33069fafca03d0b5a3397cf1822c959d4
Chris Ventura <45495992+nrcventura@users.noreply.github.com>
2021-02-02 17:25:34 -0800

    Fix request_test.go

--- telemetry/request_test.go
+++ telemetry/request_test.go
@@ -5,80 +5,212 @@ package telemetry
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
 	"testing"
+	"github.com/newrelic/newrelic-telemetry-sdk-go/internal"
 )
 
-type testRequestBuilder struct {
-	bodies []json.RawMessage
+type testUnsplittablePayloadEntry struct {
+	rawData json.RawMessage
 }
 
-func (ts testRequestBuilder) makeBody() json.RawMessage {
-	return ts.bodies[0]
+func (p *testUnsplittablePayloadEntry) Type() string {
+	return "testUnsplittable"
 }
 
-func (ts testRequestBuilder) split() []requestsBuilder {
-	reqs := ts.bodies[1:]
-	if len(reqs) == 0 {
+func (p *testUnsplittablePayloadEntry) Bytes() []byte {
+	return p.rawData
+}
+
+type testSplittablePayloadEntry struct {
+	rawData json.RawMessage
+	splitPayloads []*testSplittablePayloadEntry
+}
+
+func (p *testSplittablePayloadEntry) Type() string {
+	return "testSplittable"
+}
+
+func (p *testSplittablePayloadEntry) Bytes() []byte {
+	return p.rawData
+}
+
+func (p *testSplittablePayloadEntry) split() []splittablePayloadEntry {
+	if (nil == p.splitPayloads) {
 		return nil
 	}
-	return []requestsBuilder{
-		testRequestBuilder{bodies: reqs},
-		testRequestBuilder{bodies: reqs},
+
+	splitPayloads := make([]splittablePayloadEntry, len(p.splitPayloads))
+	for i := 0; i < len(p.splitPayloads); i++ {
+		splitPayloads[i] = p.splitPayloads[i]
+	}
+	return splitPayloads
+}
+
+func TestInvalidUrlResultsInError(t *testing.T) {
+	testPayload := testUnsplittablePayloadEntry{rawData: json.RawMessage(`123456789`)}
+	entries := []PayloadEntry{&testPayload}
+	reqs, err := newRequestsInternal(entries, "apikey", "\n", "testua", func(r http.Request) bool {
+		return false
+	})
+	if err == nil {
+		t.Error("Expected a url parsing error but didn't get one.")
+	}
+	if reqs != nil {
+		t.Error("No requests should have been generated.")
 	}
 }
 
-func TestNewRequestsSplitSuccess(t *testing.T) {
-	ts := testRequestBuilder{
-		bodies: []json.RawMessage{
-			json.RawMessage(`12345678901234567890`),
-			json.RawMessage(`123456789012345`),
-			json.RawMessage(`12345678901`),
-			json.RawMessage(`123456789`),
-		},
-	}
-	reqs, err := newRequestsInternal(ts, "", "", "", func(r request) bool {
-		return len(r.UncompressedBody) >= 10
+func TestNewRequestNoSplitNeeded(t *testing.T) {
+	testPayload := testUnsplittablePayloadEntry{rawData: json.RawMessage(`123456789`)}
+	entries := []PayloadEntry{&testPayload}
+	reqs, err := newRequestsInternal(entries, "apikey", "https://myhost/mypath", "testua", func(r http.Request) bool {
+		return false
 	})
 	if err != nil {
 		t.Error(err)
 	}
-	if len(reqs) != 8 {
+	if len(reqs) != 1 {
 		t.Error(len(reqs))
 	}
 }
 
-func TestNewRequestsCantSplit(t *testing.T) {
-	ts := testRequestBuilder{
-		bodies: []json.RawMessage{
-			json.RawMessage(`12345678901234567890`),
-			json.RawMessage(`123456789012345`),
-			json.RawMessage(`12345678901`),
+func TestNewRequestSplitNeeded(t *testing.T) {
+	testPayload := testSplittablePayloadEntry{
+		rawData: json.RawMessage(`"123456789"`),
+		splitPayloads: []*testSplittablePayloadEntry{
+			{rawData: json.RawMessage(`"1234"`)},
+			{rawData: json.RawMessage(`"56789"`)},
 		},
 	}
-	reqs, err := newRequestsInternal(ts, "", "", "", func(r request) bool {
-		return len(r.UncompressedBody) >= 10
+	entries := []PayloadEntry{&testPayload}
+	reqs, err := newRequestsInternal(entries, "apikey", "https://myhost/mypath", "testua", func(r http.Request) bool {
+		shouldSplit, err := payloadContains(r, "testSplittable", "123456789")
+
+		if (nil != err) {
+			t.Error(err)
+		}
+
+		return shouldSplit
 	})
+	if err != nil {
+		t.Error(err)
+	}
+	if len(reqs) != 2 {
+		t.Error(len(reqs))
+	}
+}
+
+func TestNewRequestSplittingMultiplePayloadsNeeded(t *testing.T) {
+	testUnsplittablePayloadEntry := testUnsplittablePayloadEntry{
+		rawData: json.RawMessage(`"abc"`),
+	}
+	testSplittablePayload := testSplittablePayloadEntry{
+		rawData: json.RawMessage(`"123456789"`),
+		splitPayloads: []*testSplittablePayloadEntry{
+			{rawData: json.RawMessage(`"1234"`)},
+			{rawData: json.RawMessage(`"56789"`)},
+		},
+	}
+	entries := []PayloadEntry{&testUnsplittablePayloadEntry, &testSplittablePayload}
+	reqs, err := newRequestsInternal(entries, "apikey", "https://myhost/mypath", "testua", func(r http.Request) bool {
+		shouldSplit, err := payloadContains(r, "testSplittable", "123456789")
+
+		if (nil != err) {
+			t.Error(err)
+		}
+
+		return shouldSplit
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if len(reqs) != 2 {
+		t.Error(len(reqs))
+	}
+
+	expectedSplitPayloads := []string{"1234", "56789"}
+	for i := 0; i < 2; i++ {
+		hasUnsplittablePayload, err := payloadContains(reqs[i], "testUnsplittable", "abc")
+		if (err != nil) {
+			t.Error(err)
+		}
+		if (!hasUnsplittablePayload) {
+			t.Error("Each request should contain the unsplittable payload")
+		}
+
+		hasSplittablePayload, err := payloadContains(reqs[i], "testSplittable", expectedSplitPayloads[i])
+		if (err != nil) {
+			t.Error(err)
+		}
+		if (!hasSplittablePayload) {
+			t.Errorf("testSplittable did not contain %q", expectedSplitPayloads[i])
+		}
+	}
+}
+
+func TestNewRequestCantSplitPayload(t *testing.T) {
+	testPayload := testSplittablePayloadEntry{
+		rawData: json.RawMessage(`"123456789"`),
+	}
+	entries := []PayloadEntry{&testPayload}
+	reqs, err := newRequestsInternal(entries, "apikey", "https://myhost/mypath", "testua", func(r http.Request) bool {
+		shouldSplit, err := payloadContains(r, "testSplittable", "123456789")
+
+		if (nil != err) {
+			t.Error(err)
+		}
+
+		return shouldSplit
+	})
+
 	if err != errUnableToSplit {
 		t.Error(err)
 	}
-	if len(reqs) != 0 {
-		t.Error(len(reqs))
+	if reqs != nil {
+		t.Error("reqs should be nil")
 	}
 }
 
-func randomJSON(numBytes int) json.RawMessage {
-	digits := []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-	js := make([]byte, numBytes)
-	for i := 0; i < len(js); i++ {
-		js[i] = digits[rand.Intn(len(digits))]
+func TestNewRequestCantSplitPayloadsEnough(t *testing.T) {
+	testPayload := testSplittablePayloadEntry{
+		rawData: json.RawMessage(`"123456789"`),
+		splitPayloads: []*testSplittablePayloadEntry{
+			{rawData: json.RawMessage(`"1234"`)},
+			{rawData: json.RawMessage(`"56789"`)},
+		},
 	}
-	return js
+	entries := []PayloadEntry{&testPayload}
+	reqs, err := newRequestsInternal(entries, "apikey", "https://myhost/mypath", "testua", func(r http.Request) bool {
+		isOriginalPayload, err := payloadContains(r, "testSplittable", "123456789")
+
+		if (nil != err) {
+			t.Error(err)
+		}
+
+		isPayloadThatCantBeSplitAgain, err := payloadContains(r, "testSplittable", "56789")
+
+		if (nil != err) {
+			t.Error(err)
+		}
+
+		return isOriginalPayload || isPayloadThatCantBeSplitAgain
+	})
+
+	if err != errUnableToSplit {
+		t.Error(err)
+	}
+	if reqs != nil {
+		t.Error("reqs should be nil")
+	}
 }
 
 func TestLargeRequestNeedsSplit(t *testing.T) {
 	js := randomJSON(4 * maxCompressedSizeBytes)
-	reqs, err := newRequests(testRequestBuilder{bodies: []json.RawMessage{js}}, "apiKey", defaultMetricURL, "userAgent")
+	payloadEntry := testUnsplittablePayloadEntry{rawData: js}
+	reqs, err := newRequests([]PayloadEntry{&payloadEntry}, "apiKey", defaultMetricURL, "userAgent")
 	if reqs != nil {
 		t.Error(reqs)
 	}
@@ -89,7 +221,8 @@ func TestLargeRequestNeedsSplit(t *testing.T) {
 
 func TestLargeRequestNoSplit(t *testing.T) {
 	js := randomJSON(maxCompressedSizeBytes / 2)
-	reqs, err := newRequests(testRequestBuilder{bodies: []json.RawMessage{js}}, "apiKey", defaultMetricURL, "userAgent")
+	payloadEntry := testUnsplittablePayloadEntry{rawData: js}
+	reqs, err := newRequests([]PayloadEntry{&payloadEntry}, "apiKey", defaultMetricURL, "userAgent")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -97,10 +230,26 @@ func TestLargeRequestNoSplit(t *testing.T) {
 		t.Fatal(len(reqs))
 	}
 	req := reqs[0]
-	if u := req.Request.URL.String(); u != defaultMetricURL {
+	if u := req.URL.String(); u != defaultMetricURL {
 		t.Error(u)
 	}
-	if len(req.UncompressedBody) != maxCompressedSizeBytes/2 {
-		t.Error(len(req.UncompressedBody))
+}
+
+func payloadContains(r http.Request, fieldName string, value string) (bool, error) {
+	bodyReader, _ := r.GetBody()
+	compressedBytes, _ := ioutil.ReadAll(bodyReader)
+	uncompressedBytes, _ := internal.Uncompress(compressedBytes)
+	var entry []map[string]string
+	err := json.Unmarshal(uncompressedBytes, &entry)
+
+	return entry[0][fieldName] == value, err
+}
+
+func randomJSON(numBytes int) json.RawMessage {
+	digits := []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	js := make([]byte, numBytes)
+	for i := 0; i < len(js); i++ {
+		js[i] = digits[rand.Intn(len(digits))]
 	}
+	return js
 }
